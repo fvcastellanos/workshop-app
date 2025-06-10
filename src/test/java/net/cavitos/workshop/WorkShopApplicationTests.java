5dd6a4b827fb3e10cd0544229aaf8e27293edd72
Francisco Castellanos <fvcastellanos@gmail.com>
2025-06-09 19:53:54 -0600

    WE-36: add context configuration

--- src/test/java/net/cavitos/workshop/WorkShopApplicationTests.java
+++ src/test/java/net/cavitos/workshop/WorkShopApplicationTests.java
@@ -2,8 +2,10 @@ package net.cavitos.workshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(locations = "net.cavitos.workshop")
 class WorkShopApplicationTests {
 
 	@Test
